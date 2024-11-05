3038a94dfd38d27c3a61345ca181d8c921498626
Sameer khan <121482820+Sameerkhan9412@users.noreply.github.com>
2024-11-05 09:27:52 +0530

    Queue ques folde added

---
+++ 10_Queue/ques/01_Reverse_A_Queue.cpp
@@ -0,0 +1,49 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+
+void reverseQueue(queue<int> &q){
+    stack<int> st;
+    while(!q.empty()){
+        int Element=q.front();
+        q.pop();
+        st.push(Element);
+    }
+    // one by one print element
+    while(!st.empty()){
+        int element=st.top();
+        q.push(element);
+        st.pop();
+    }
+}
+
+void usingRecursion(queue<int> &q){
+    if(q.empty()){
+        return;
+    }
+    int element=q.front();
+    q.pop();
+    usingRecursion(q);
+    q.push(element);
+}
+
+int main() {
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    // reverseQueue(q);
+    // using recursion
+    usingRecursion(q);
+    cout<<"Printitng queue:"<<endl;
+    while(!q.empty()){
+        int element=q.front();
+        cout<<element<<" ";
+        q.pop();
+    }
+    return 0;
+}
